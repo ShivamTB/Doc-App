1d16c3d25b6a21d7eeb111af6f79a28ab4416297
Shivam Agarwal <nakul1010@gmail.com>
2018-03-31 16:40:07 +0530

    fixed fetch

--- first_app/views.py
+++ first_app/views.py
@@ -73,7 +73,7 @@ def patient_update(request, pk):
 
 def patient_fetch(request, pk):
     patient = get_object_or_404(Patient, pk=pk)
-    return (request, 'first_app/index.html', {'patient':patient})
+    return render(request, 'first_app/index.html', {'patient':patient})
 
 def patient_delete(request, pk):
     patient = get_object_or_404(Patient, pk=pk)
